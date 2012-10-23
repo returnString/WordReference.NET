8fd1bf00ca7fa6f34d5636148aeed7da99f5ac45
Ruan PA <ruan@inkdev.net>
2012-10-23 02:09:29 +0100

    Minor cleanup

--- WordReference.NET/LanguageDictionary.cs
+++ WordReference.NET/LanguageDictionary.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
